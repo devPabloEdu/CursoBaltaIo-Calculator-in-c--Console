a0a6b8512064878c0d0ca718398822ba01b15494
Pablo Eduardo <158394108+devPabloEdu@users.noreply.github.com>
2024-06-04 21:13:50 -0300

    testando concatenação e conversões

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Calculator
+ {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear(); //ele limpa a tela
+            Console.WriteLine("PRIMEIRO VALOR: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("SEU NOME: ");
+            string nome = Console.ReadLine();
+            
+
+            Console.WriteLine("ola " + nome + " seu saldo é de " + v1);
+        }
+    }
+ }
