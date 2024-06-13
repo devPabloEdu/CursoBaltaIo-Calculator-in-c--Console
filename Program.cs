a51626ed52f574c1457581c818e893082f2b1d6a
Devpabloedu <158394108+devPabloEdu@users.noreply.github.com>
2024-06-13 15:49:15 -0300

    .

--- Program.cs
+++ Program.cs
@@ -7,10 +7,13 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.Clear(); //ele limpa a tela
+
             Console.WriteLine("PRIMEIRO VALOR: ");
             float v1 = float.Parse(Console.ReadLine());
+
             Console.WriteLine("SEGUNDO VALOR:");
-            float v2 = float.Parse(Console.ReadLine());    
+            float v2 = float.Parse(Console.ReadLine());   
+             
             float resultado = v1 + v2;    
             Console.WriteLine("");   
             Console.WriteLine("O resultado da soma é: " + resultado);
